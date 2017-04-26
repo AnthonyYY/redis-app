960bdf08fbb45fed222e335833fdf30481f3741a
AnthonyYY <yjh2332@gmail.com>
2017-04-26 21:21:27 +0800

    add config.js and user-login.ejs

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  port: 3000,
+  secret: 'secret',
+  redisUrl: 'redis://localhost',
+  routes: {
+    login: '/login',
+    logout: '/logout'
+  }
+};
+
+module.exports = config;
